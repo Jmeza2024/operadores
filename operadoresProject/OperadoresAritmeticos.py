cf1990d45e8f81cd5d1274a2268129f428fb17cb
Camilo Meza <juancamilomezatapia11@gmail.com>
2024-09-26 16:51:05 -0500

    se crea el prpyecto con los ejemplos de los operadores

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,9 @@
+#Ejemplos utilizando operadores aritmeticos
+numero1 = 18
+numero2 = 12
+
+suma = numero1 + numero2
+print(f"la suma de {numero1} y {numero2} es: {suma} ")
+
+
+
---
+++ operadoresProject/resta.py
@@ -0,0 +1,5 @@
+numero1 = 18
+numero2 = 12
+resta = numero1 - numero2
+
+print("la resta es",str(resta))
